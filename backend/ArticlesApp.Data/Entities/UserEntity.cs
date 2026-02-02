777fcfdb07c691120f94f41acb1e4830c967e9d7
EduardoCR1994 <133717717+EduardoCR1994@users.noreply.github.com>
2026-02-02 09:29:16 -0600

    Add initial data entities, domain DTOs and database scripts

---
+++ backend/ArticlesApp.Data/Entities/UserEntity.cs
@@ -0,0 +1,17 @@
+﻿namespace ArticlesApp.Data.Entities
+{
+    public class UserEntity
+    {
+        public int Id { get; set; }
+
+        public string Username { get; set; } = null!;
+
+        public string PasswordHash { get; set; } = null!;
+
+        public bool IsActive { get; set; } = true;
+
+        public DateTime CreatedAt { get; set; }
+
+        public int? CreatedBy { get; set; }
+    }
+}
